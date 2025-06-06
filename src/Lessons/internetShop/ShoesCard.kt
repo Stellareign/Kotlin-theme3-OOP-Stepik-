04d604634ec10cc6e7f60b7cb6fa10cf321ad2c9
Stellareign <tuguran@mail.ru>
2025-06-06 23:49:32 +0300

    theme 3.13 productCards

--- src/Lessons/internetShop/ShoesCard.kt
+++ src/Lessons/internetShop/ShoesCard.kt
@@ -1,20 +1,24 @@
 package Lessons.internetShop
 
+import jdk.jfr.Description
+
 class ShoesCard(
     productName: String,
     brand: String,
     price: Double,
     type: String,
-    val size: Double
+    val size: Double,
+    description : String
 ) : ProductCard (
     productName = productName,
     brand = brand,
     price = price,
     type = type,
+    description = description
 ) {
     override fun printInfo() {
-        super.printInfo()
-        println("Size : $size");
+        super.printInfo();
+        println("\nSize : $size");
     }
 
 }
