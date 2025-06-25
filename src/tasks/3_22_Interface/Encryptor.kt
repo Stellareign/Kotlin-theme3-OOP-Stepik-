7de3d26172ff27865596fccad61040caecf417d5
Stellareign <tuguran@mail.ru>
2025-06-25 21:37:47 +0300

    interface задачка

--- src/tasks/3_22_Interface/Encryptor.kt
+++ src/tasks/3_22_Interface/Encryptor.kt
@@ -1,11 +1,12 @@
 package tasks.`3_22_Interface`
 
 class Encryptor : DataProcessor(), Transformable {
-    val cl = Encryptor :: class.java
+    val cl = (Encryptor :: class.java).simpleName
+
     override fun transform(data: String): String {
-       return "${cl.simpleName} преобразовал данные: encoded_$data";
+       return "${cl} преобразовал данные: encoded_$data";
     }
     override fun process(data: String): String {
-       return "Обработчик: ${cl.simpleName} обработал данные";
+       return "Обработчик: ${cl} обработал данные";
     }
 }
